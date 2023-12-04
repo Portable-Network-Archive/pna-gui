2f09bf8e284d880c9a0ac038ac7eec90120251d6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-12-04 23:14:04 +0900

    :zap: Add check for update in menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,9 @@
 
 use std::{fs, io, path::Path};
 
-use tauri::Window;
+#[cfg(not(target_os = "macos"))]
+use tauri::Submenu;
+use tauri::{CustomMenuItem, Menu, MenuEntry, Window};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -44,9 +46,38 @@ fn _extract(window: Window, path: &str) -> io::Result<()> {
     Ok(())
 }
 
+const MENU_UPDATE_CHECK: &str = "update check";
+
 fn main() {
+    let context = tauri::generate_context!();
+    let mut menu = Menu::os_default(&context.package_info().name);
+    let update_check = CustomMenuItem::new(MENU_UPDATE_CHECK, "Check for updates...");
+    #[cfg(target_os = "macos")]
+    if let MenuEntry::Submenu(sub_menu) = &mut menu.items[0] {
+        sub_menu
+            .inner
+            .items
+            .insert(1, MenuEntry::CustomItem(update_check));
+    }
+    #[cfg(not(target_os = "macos"))]
+    {
+        menu = menu.add_submenu(Submenu::new("Tools", Menu::new().add_item(update_check)));
+    }
+
     tauri::Builder::default()
+        .menu(menu)
+        .on_menu_event(|event| {
+            match event.menu_item_id() {
+                MENU_UPDATE_CHECK => {
+                    event
+                        .window()
+                        .emit_and_trigger("tauri://update", ())
+                        .unwrap();
+                }
+                m => println!("{}", m),
+            };
+        })
         .invoke_handler(tauri::generate_handler![greet, extract])
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
