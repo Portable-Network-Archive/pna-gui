3de2b80faebb912909c6c1c71b61f10e70907f2b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-12-13 22:24:50 +0900

    :sparkles: Add `Extract` in menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Extract, Create } from "./tabs";
+import { appWindow } from "@tauri-apps/api/window";
 import "./App.css";
 
 type Mode = "extract" | "create";
 
 function App() {
   const [mode, setMode] = useState<Mode>("extract");
+  useEffect(() => {
+    const unlisten = appWindow.listen<void>("open_extract", () => {
+    });
+    return () => {
+      unlisten.then((it) => it());
+    };  
+  }, []);
 
   return (
     <div className="container">
