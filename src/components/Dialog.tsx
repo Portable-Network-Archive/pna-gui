61c48123da55f88b2405b203d95528b963cf09a6
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-03-02 17:57:48 +0900

    :zap: use `React.forwardRef` for Dialog components

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -6,52 +6,66 @@ export const Root: React.FC<Dialog.DialogProps> = ({ ...props }) => (
   <Dialog.Root {...props} />
 );
 
-export const Trigger: React.FC<
-  Dialog.DialogTriggerProps & React.RefAttributes<HTMLButtonElement>
-> = ({ className, ...props }) => (
+export const Trigger = React.forwardRef<
+  HTMLButtonElement,
+  Dialog.DialogTriggerProps
+>(({ className, ...props }, ref) => (
   <Dialog.Trigger
     className={`${styles.DialogTrigger} ${className}`}
     {...props}
+    ref={ref}
   />
-);
+));
 
 export const Portal: React.FC<Dialog.PortalProps> = ({ ...props }) => (
   <Dialog.Portal {...props} />
 );
 
-export const Overlay: React.FC<
-  Dialog.DialogOverlayProps & React.RefAttributes<HTMLDivElement>
-> = ({ className, ...props }) => (
+export const Overlay = React.forwardRef<
+  HTMLDivElement,
+  Dialog.DialogOverlayProps
+>(({ className, ...props }, ref) => (
   <Dialog.Overlay
     className={`${styles.DialogOverlay} ${className}`}
     {...props}
+    ref={ref}
   />
-);
+));
 
-export const Content: React.FC<
-  Dialog.DialogContentProps & React.RefAttributes<HTMLDivElement>
-> = ({ className, ...props }) => (
+export const Content = React.forwardRef<
+  HTMLDivElement,
+  Dialog.DialogContentProps
+>(({ className, ...props }, ref) => (
   <Dialog.Content
     className={`${styles.DialogContent} ${className}`}
     {...props}
+    ref={ref}
   />
-);
+));
 
-export const Title: React.FC<
-  Dialog.DialogTitleProps & React.RefAttributes<HTMLHeadingElement>
-> = ({ className, ...props }) => (
-  <Dialog.Title className={`${styles.DialogTitle} ${className}`} {...props} />
-);
+export const Title = React.forwardRef<
+  HTMLHeadingElement,
+  Dialog.DialogTitleProps
+>(({ className, ...props }, ref) => (
+  <Dialog.Title
+    className={`${styles.DialogTitle} ${className}`}
+    {...props}
+    ref={ref}
+  />
+));
 
-export const Description: React.FC<
-  Dialog.DialogDescriptionProps & React.RefAttributes<HTMLParagraphElement>
-> = ({ className, ...props }) => (
+export const Description = React.forwardRef<
+  HTMLParagraphElement,
+  Dialog.DialogDescriptionProps
+>(({ className, ...props }, ref) => (
   <Dialog.Description
     className={`${styles.DialogDescription} ${className}`}
     {...props}
+    ref={ref}
   />
-);
+));
 
-export const Close: React.FC<
-  Dialog.DialogCloseProps & React.RefAttributes<HTMLButtonElement>
-> = ({ ...props }) => <Dialog.Close {...props} />;
+export const Close = React.forwardRef<
+  HTMLButtonElement,
+  Dialog.DialogCloseProps
+>(({ ...props }, ref) => <Dialog.Close {...props} ref={ref} />);
